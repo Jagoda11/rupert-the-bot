50bb200f0d5a1ddaed7fe044d87dfc542f97c074
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-05-15 00:27:57 +0200

    chore: Update .gitignore and jest.config.cjs
    - Update .gitignore to include .aws-sam/ directory
    - Update jest.config.cjs to ignore /node_modules/ and /.aws-sam/ directories

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,8 @@
 // jest.config.cjs
 module.exports = {
   testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json'],
+  testPathIgnorePatterns: ['/node_modules/', '/.aws-sam/'],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
